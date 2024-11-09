f82d6209c0733e4cebc4281061893c2540fd0843
kaanguru <kaanguru@mail.ru>
2024-11-09 15:09:52 +0300

    update 2 files and delete 1 file

--- website/src/navigation.js
+++ website/src/navigation.js
@@ -1,11 +1,4 @@
-export const headerData = {
-  links: [
-    {
-      text: 'docs',
-      href: '/docs/',
-    },
-  ],
-};
+export const headerData = {};
 export const footerData = {
   socialLinks: [
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/kaanguru/lock4it/stargazers' },
