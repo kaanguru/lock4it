4ea10a9691ded316d2974921d771e3c690e2dffa
kaanguru <kaanguru@mail.ru>
2024-11-14 18:17:36 +0300

    fix i18n

--- app/src/lib/i18n/index.ts
+++ app/src/lib/i18n/index.ts
@@ -1,8 +1,14 @@
 import { addMessages, init, getLocaleFromNavigator } from 'svelte-i18n';
 import en from './locales/en.json';
+import ar from './locales/ar.json';
+import ru from './locales/ru.json';
 import tr from './locales/tr.json';
+import zh from './locales/zh.json';
 addMessages('en', en);
+addMessages('ar', ar);
+addMessages('ru', ru);
 addMessages('tr', tr);
+addMessages('zh', zh);
 
 init({
 	fallbackLocale: 'en',
